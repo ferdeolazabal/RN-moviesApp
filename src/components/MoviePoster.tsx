4aa1b45b8792cc97e840fbd216ae3bd59054931d
ferdeolazabal <ferdeolazabal@gmail.com>
2023-01-07 18:01:42 -0300

    Add initial config to insert picture movies carousel
    - Install some packages
    - Config at babel.config

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
+import { Movie } from '../interfaces/movieInterfaces';
+interface Props {
+    movie: Movie;
+}
 
-export const MoviePoster = () => {
+export const MoviePoster = ({ movie }: Props) => {
+    const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
     return (
-        <View>
-            <Text>MoviePoster</Text>
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image source={{ uri }} style={styles.image} />
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        width: 300,
+        height: 420,
+    },
+    image: {
+        flex: 1,
+        borderRadius: 20,
+    },
+    imageContainer: {
+        flex: 1,
+        borderRadius: 18,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.62,
+        elevation: 10,
+    },
+});
 
 export default MoviePoster;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { View, Text, Button, ActivityIndicator } from 'react-native';
+import Carousel from 'react-native-reanimated-carousel';
 import { StackScreenProps } from '@react-navigation/stack';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 import { UseMovies } from '../hooks/UseMovies';
+import { View, ActivityIndicator, Dimensions } from 'react-native';
 import MoviePoster from '../components/MoviePoster';
 
 interface Props extends StackScreenProps<any, any> {}
+const { width: windowWidth } = Dimensions.get('window');
 
 const HomeScreen = ({ navigation }: Props) => {
     const { actualMovies, isLoading } = UseMovies();
+    const { top } = useSafeAreaInsets();
 
     if (isLoading) {
         return (
@@ -18,15 +23,34 @@ const HomeScreen = ({ navigation }: Props) => {
     }
 
     return (
-        <View>
-            <MoviePoster />
-            {/* <Text
-                style={{
-                    color: 'black',
-                }}>
-                Home
-            </Text>
-            <Button title="Go to Details" onPress={() => navigation.navigate('DetailScreen')} /> */}
+        <View style={{ marginTop: top + 20 }}>
+            <View style={{ height: 440 }}>
+                <Carousel
+                    // Just one of the many styles from the Carousel module
+                    mode="parallax"
+                    // This style prop regards to the carousel container not to the item itself
+                    style={{ width: windowWidth, justifyContent: 'center' }}
+                    // Paging in false allows to do superfast scroll
+                    pagingEnabled={false}
+                    // and that superfast scroll stops on multiples of windowSize
+                    windowSize={2}
+                    // the snap helps to stop exactly in 1 item, no in the middle of two or so
+                    snapEnabled
+                    // This props are for the item in the middle
+                    width={300}
+                    height={420}
+                    modeConfig={{
+                        // How the "main" item will look
+                        parallaxScrollingScale: 0.9,
+                        // How separate the adjacent items will be
+                        parallaxScrollingOffset: 40,
+                        // How big the adjacent items will look compared to the "main" item
+                        parallaxAdjacentItemScale: 0.75,
+                    }}
+                    data={actualMovies}
+                    renderItem={({ item }) => <MoviePoster movie={item} />}
+                />
+            </View>
         </View>
     );
 };
