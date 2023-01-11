78ab20f163457f72fb23a60a43072c8755287df8
ferdeolazabal <ferdeolazabal@gmail.com>
2023-01-10 21:27:08 -0300

    Add initial detail screeen
    - in progress
    - Add route to detail screen
    - Add props of routes component

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { Movie } from '../interfaces/movieInterfaces';
+import { useNavigation } from '@react-navigation/native';
+
 interface Props {
     movie: Movie;
     height?: number;
@@ -10,12 +12,17 @@ interface Props {
 export const MoviePoster = ({ movie, height = 420, width = 300 }: Props) => {
     const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
+    const navigation = useNavigation<any>();
+
     return (
-        <View style={{ height, width, marginHorizontal: 7 }}>
+        <TouchableOpacity
+            onPress={() => navigation.navigate('DetailScreen', movie)}
+            activeOpacity={0.8}
+            style={{ height, width, marginHorizontal: 2, paddingBottom: 20, paddingHorizontal: 2 }}>
             <View style={styles.imageContainer}>
                 <Image source={{ uri }} style={styles.image} />
             </View>
-        </View>
+        </TouchableOpacity>
     );
 };
 
--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,21 +1,43 @@
-import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
+import { View, Text, Button, StyleSheet, Image, Dimensions } from 'react-native';
+import { RootStackParams } from '../navigation/NavigationController';
 
-interface Props extends StackScreenProps<any, any> {}
+interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'> {}
+const { height: screenHeight } = Dimensions.get('screen');
 
-const DetailScreen = ({ navigation }: Props) => {
+const DetailScreen = ({ navigation, route }: Props) => {
+    const movie = route.params;
+
+    const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
     return (
-        <View>
-            <Text
-                style={{
-                    color: 'black',
-                }}>
-                DetailScreen
-            </Text>
+        // <View>
+        //     <Text
+        //         style={{
+        //             color: 'black',
+        //         }}>
+        //         DetailScreen
+        //     </Text>
 
-            <Button title="Go to Home" onPress={() => navigation.navigate('HomeScreen')} />
+        //     <Button title="Go to Home" onPress={() => navigation.navigate('HomeScreen')} />
+        <View style={styles.imageContainer}>
+            <Image source={{ uri }} style={styles.posterImage} />
         </View>
+        // </View>
     );
 };
 export default DetailScreen;
+
+const styles = StyleSheet.create({
+    imageContainer: {
+        width: '100%',
+        height: screenHeight * 0.7,
+    },
+    posterImage: {
+        flex: 1,
+        borderRadius: 20,
+        width: 300,
+        height: 420,
+    },
+});
