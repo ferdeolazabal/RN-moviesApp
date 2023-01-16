45f75d64f52c387eabd68b93e78bba9b183a0f1b
ferdeolazabal <ferdeolazabal@gmail.com>
2023-01-16 19:40:20 -0300

    delete comments}
    - Add ts types for icons

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -58,25 +58,16 @@ const HomeScreen = ({ navigation }: Props) => {
                     </Text>
                     <View style={{ height: 405 }}>
                         <Carousel
-                            // Just one of the many styles from the Carousel module
                             mode="parallax"
-                            // This style prop regards to the carousel container not to the item itself
                             style={{ width: windowWidth, justifyContent: 'center' }}
-                            // Paging in false allows to do superfast scroll
                             pagingEnabled={false}
-                            // and that superfast scroll stops on multiples of windowSize
                             windowSize={2}
-                            // the snap helps to stop exactly in 1 item, no in the middle of two or so
                             snapEnabled
-                            // This props are for the item in the middle
                             width={300}
                             height={420}
                             modeConfig={{
-                                // How the "main" item will look
                                 parallaxScrollingScale: 0.9,
-                                // How separate the adjacent items will be
                                 parallaxScrollingOffset: 40,
-                                // How big the adjacent items will look compared to the "main" item
                                 parallaxAdjacentItemScale: 0.75,
                             }}
                             data={nowPlaying}
